ec3f8c39597a3acf713a4e33c2dc7fd0a2fd9d47
Abuzar Khan <149166915+abuzarkhanse@users.noreply.github.com>
2025-04-27 10:52:53 +0000

    Spring Boot Project Commit

---
+++ snippetsearch/src/test/snippetsearch/SnippetSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snippetsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
