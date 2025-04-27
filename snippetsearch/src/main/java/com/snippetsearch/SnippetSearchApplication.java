95f52342bd7602875af45ce2065500be0be70784
Abuzar Khan <149166915+abuzarkhanse@users.noreply.github.com>
2025-04-27 10:39:57 +0000

    Download Spring Boot Project

---
+++ snippetsearch/src/main/java/com/snippetsearch/SnippetSearchApplication.java
@@ -0,0 +1,13 @@
+package com.snippetsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetSearchApplication.class, args);
+	}
+
+}
