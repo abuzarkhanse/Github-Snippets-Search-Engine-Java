22c88e45561d2f44c0fbd67b616c0cb13a578d37
Sufyan Hummam <131873568+sufyan-hummam@users.noreply.github.com>
2025-04-27 17:36:00 +0000

    backend project model commit

---
+++ snippetsearch/src/main/java/com/snippetsearch/model/Snippet.java
@@ -0,0 +1,40 @@
+package com.snippetsearch.model;
+
+public class Snippet {
+    private String filename;
+    private String code;
+    private String language;
+    private String repoUrl;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getRepoUrl() {
+        return repoUrl;
+    }
+
+    public void setRepoUrl(String repoUrl) {
+        this.repoUrl = repoUrl;
+    }
+}
